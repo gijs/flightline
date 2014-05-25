e339b1d5e98c80386ee860d5f70e1d6ccac70faa
Fatih Kalifa <fatih@pvey.es>
2014-05-25 13:50:39 -0700

    Change template to script tag, add queue mode

--- js/ui/timeline.js
+++ js/ui/timeline.js
@@ -3,12 +3,22 @@
 
 define([
     'flight/lib/component',
-    'text!templates/tweet.html',
     '../utils',
     '../format'
-], function (defineComponent, tweetTmpl, utils, tweetFormatter) {
+], function (defineComponent, utils, tweetFormatter) {
+    var parseTemplate = function (id) {
+        var tmpl = document.querySelector(id).innerHTML
+        return utils.tmpl(tmpl);
+    }
+
     function timeline() {
-        var template = utils.tmpl(tweetTmpl);
+        var tweetTmpl = parseTemplate('#tweet-template'),
+            tweetQueueTmpl = parseTemplate('#tweet-queue-template'),
+            newTweetQueue = [];
+
+        this.defaultAttrs({
+            queueSelector: '#tweet-queue'
+        });
 
         this.renderAll = function (e, data) {
             this.$node.html('');
@@ -18,12 +28,42 @@ define([
         };
 
         this.render = function (e, data) {
-            this.$node.append(template(data.tweet));
+            console.log(data.tweet);
+            var template = tweetTmpl(data.tweet)
+            this.$node.prepend(template);
+        };
+
+        this.pushToQueue = function (e, data) {
+            newTweetQueue.push(data.tweet);
+            var count = newTweetQueue.length,
+                content = { queue: count + ' new Tweet' };
+
+            if (count > 1) {
+                // already rendered, just replace innerText
+                $('#tweet-queue').text(content.queue + 's')
+                return;
+            }
+
+            var template = tweetQueueTmpl(content);
+            this.$node.prepend(template);
+        };
+
+        this.renderQueue = function (e, data) {
+            // destroy queue count
+            $('#tweet-queue').remove();
+
+            newTweetQueue.forEach(function (each) {
+                this.render(e, { tweet: each })
+            }, this);
+
+            newTweetQueue = [];
         };
 
         this.after('initialize', function () {
-            this.on(document, 'dataTweetAdded', this.render);
+            this.on(document, 'dataTweetAdded', this.pushToQueue);
             this.on(document, 'dataTweetsLoaded', this.renderAll);
+
+            this.on('click', {'queueSelector': this.renderQueue});
 
             this.trigger('uiLoadRequested');
         })
